d3eaf3b1f408b1e547a0159d305acd325eaa7a41
Sebastien Boyron <sebastien@boyron.eu>
2018-04-12 21:49:03 +0200

    Add yaml and json output format

--- niet/__main__.py
+++ niet/__main__.py
@@ -6,10 +6,15 @@ import os.path
 import yaml
 
 
+# arguments parsing
 def argparser():
     epilog = '''output formats:
-             quotes    Add quotes to result
-             '''
+  spaces    Return all elements of a list separated by spaces
+  quotes    Add quotes to result
+  newline   Return all element of a list in a new line
+  yaml      Return object in YAML
+  json      Return object in JSON
+'''
     parser = argparse.ArgumentParser(
         description='Read data from YAML or JSON file',
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -21,29 +26,34 @@ def argparser():
                         Use '.' to get whole file. \
                         eg: a.b.c")
     parser.add_argument('-f', '--format', type=str,
-                        choices=['quotes'],
+                        choices=['spaces', 'quotes',
+                                 'json', 'yaml', 'newline'],
                         help="output format")
     args = parser.parse_args()
     return args.file, args.object, args.format
 
 
+# Parsing to extract wanted object
 def data_parser(filename):
     data = None
     with open(filename, 'r') as stream:
         content = "".join(stream.readlines())
         try:
-            data = yaml.load(content)
-        except yaml.parser.ParserError:
+            data = json.loads(content)
+            in_format = "json"
+        except ValueError:
             try:
-                data = json.loads(content)
-            except ValueError as err:
+                data = yaml.load(content)
+                in_format = "yaml"
+            except yaml.parser.ParserError as err:
                 print(str(err))
     if not isinstance(data, dict):
         print("Invalid file. Only support valid json and yaml files")
         sys.exit(1)
-    return data
+    return data, in_format
 
 
+# Load file
 def get(data, keywords):
     try:
         cursor = data
@@ -61,28 +71,64 @@ def get(data, keywords):
         sys.exit(1)
 
 
-def print_result(res, out_format):
+# Output functions
+def out_print_quotes(res):
     if isinstance(res, list):
-        if 'quotes' == out_format:
-            res = " ".join(["'{}'".format(el) for el in res])
-        else:
-            res = " ".join(["{}".format(el) for el in res])
+        res = " ".join(["'{}'".format(el) for el in res])
     print(res)
 
 
+def out_print_spaces(res):
+    if isinstance(res, list):
+        res = " ".join(["{}".format(el) for el in res])
+    print(res)
+
+
+def out_print_newline(res):
+    if isinstance(res, list):
+        print('\n'.join(res))
+    else:
+        print(res)
+
+
+def out_print_yaml(res):
+    print(yaml.dump(res, default_flow_style=False))
+
+
+def out_print_json(res):
+    print(json.dumps(res, indent=4))
+
+
+def print_result(res, out_format, in_format):
+    # for compatibility with python 2 and 3
+    try:
+        basestring
+    except NameError:
+        basestring = str
+
+    if out_format is None:
+        if (isinstance(res, list) or isinstance(res, basestring) or
+                isinstance(res, int) or in_format is None):
+            out_format = "newline"
+        else:
+            out_format = in_format
+    eval("out_print_{}(res)".format(out_format))
+
+
+# Main
 def main():
     filename, search, out_format = argparser()
     if not os.path.isfile(filename):
         print("Error: File {} not found!".format(filename))
         print("Abort!")
         sys.exit(127)
-    data = data_parser(filename)
+    data, in_format = data_parser(filename)
     if not search == '.':
         keywords = search.split(".")
     else:
         keywords = search
     result = get(data, keywords)
-    print_result(result, out_format)
+    print_result(result, out_format, in_format)
 
 
 if __name__ == "__main__":
