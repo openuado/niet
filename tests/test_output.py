cddfd0f057da02fefaf720959ff1500c90b8961f
Hervé Beraud <hberaud@redhat.com>
2020-05-15 17:56:16 +0200

    [feat] add the comma output format
    These changes add the comma output format where results are represented
    on one single line and separated by commas.
    
    Also these changes:
    - fix an issue with `newline` output when elements are not instance
      of string type.
    - reindent properly the help format's part
    - rename the unit test module related to the ouput module
    
    How to test:
    ------------
    
    Setup your env:
    
    ```sh
    $ git add remote hberaud git@github.com:4383/niet
    $ git fetch hberaud
    $ git checkout -b comma hberaud/comma
    ```
    
    Now run tests:
    
    ```sh
    $ # Example of display
    $ tox -e venv -- niet "oslo.deliverables.*.repos[0]"
    https://raw.githubusercontent.com/openstack/governance/master/reference/projects.yaml
    -f comma
    $ # Example of integration with beagle and the openstack governance data
    $ tox -e venv -- beagle search -f link --repo \$\(niet
    "oslo.deliverables.*.repos[0]"
    https://raw.githubusercontent.com/openstack/governance/master/reference/projects.yaml
    -f comma\) 'venv'
    ```

---
+++ tests/test_output.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+import unittest
+
+import niet.output
+
+
+class TestPrintFunctions(unittest.TestCase):
+    def test_squotes(self):
+        output = niet.output.print_squote(['test1', 'test2'])
+        self.assertEqual("'test1' 'test2'", output)
+
+    def test_dquotes(self):
+        output = niet.output.print_dquote(['test1', 'test2'])
+        self.assertEqual('"test1" "test2"', output)
+
+    def test_newline(self):
+        output = niet.output.print_newline(['test1', 'test2'])
+        self.assertEqual('test1\ntest2', output)
+        output = niet.output.print_newline([1, 2])
+        self.assertEqual('1\n2', output)
+
+    def test_comma(self):
+        output = niet.output.print_comma(['test1', 'test2'])
+        self.assertEqual('test1,test2', output)
+        output = niet.output.print_comma([1, 2])
+        self.assertEqual('1,2', output)
